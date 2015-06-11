29c063b084037e9cac8c3e0a0d0099d287d4897d
Nathan Thompson <nthompson@helpdeskdental.com>
2015-06-11 09:07:16 -0500

    updated config.example.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+'use strict';
+
 module.exports = function(args) {
     var haha = {};
     haha.args = args;
+    haha.gcm = require('./lib/gcm');
+    console.dir(haha.gcm);
 	haha.test = function () {
         console.log('testing! ');
         return true;
